867da8a688258c8df87ed40bfca065a95875bb7d
aliyuprof100 <aliyuddeen95@gmail.com>
2025-08-04 05:28:03 -0700

    ssh file updated

--- run.sh
+++ run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
-
-# Set the FLASK_APP environment variable for this script's session
 export FLASK_APP=run:app
 export PYTHONPATH=$(pwd)
-
-
-# Run Flask database migrations
-echo "Running database migrations..."
 flask db upgrade
-
-# Start the Gunicorn server
-echo "Starting Gunicorn server..."
-gunicorn run:app
+exec gunicorn run:app
