3290ee0d7c2b0970111e5c9f0bfd76c74fd1e963
aliyuprof100 <aliyuddeen95@gmail.com>
2025-08-04 05:21:43 -0700

    Build: Trivial change to trigger fresh Render deploy

--- run.sh
+++ run.sh
@@ -5,11 +5,13 @@ set -e
 
 # Set the FLASK_APP environment variable for this script's session
 export FLASK_APP=run:app
+export PYTHONPATH=$(pwd)
+
 
 # Run Flask database migrations
 echo "Running database migrations..."
 flask db upgrade
 
-# Start the Gunicorn server, pointing to the app object in run.py
+# Start the Gunicorn server
 echo "Starting Gunicorn server..."
 gunicorn run:app
