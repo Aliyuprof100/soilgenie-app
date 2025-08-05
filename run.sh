052d8f71c3ae1d05c766eea156263f46429e5e34
aliyuprof100 <aliyuddeen95@gmail.com>
2025-08-05 22:02:42 +0100

    Run.sh edited

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# This script is designed to run from the root of the project.
 set -e
 
-# Tell the Flask CLI where to find the application factory
+# Tell the Flask CLI that the app is inside the 'soilgenie' package
+# and to find it using the factory pattern. This is the most robust method.
 export FLASK_APP="soilgenie:create_app()"
 
-# Run Flask database migrations
-echo "Running database migrations..."
+# Tell Gunicorn to do the same, but also explicitly add the current
+# directory to the Python path to resolve any ambiguity.
+# The format is wsgi:app (filename:variable_name). We will use run.py
+# as the entry point for gunicorn.
+
+# Run Flask database migrations first.
+echo "==> Running database migrations..."
 flask db upgrade
 
-# Start the Gunicorn server pointing to the wsgi.py file
-echo "Starting Gunicorn server..."
-gunicorn wsgi:app
+# Now, start the Gunicorn server.
+echo "==> Starting Gunicorn server..."
+gunicorn --pythonpath . -w 4 run:app
