218269c8b782a755c6f41463c77310eef8d08a47
aliyuprof100 <aliyuddeen95@gmail.com>
2025-08-05 22:11:05 +0100

    Build: Final attempt with cd into src directory

--- run.sh
+++ run.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-
-# This script is designed to run from the root of the project.
 set -e
 
-# Tell the Flask CLI that the app is inside the 'soilgenie' package
-# and to find it using the factory pattern. This is the most robust method.
-export FLASK_APP="soilgenie:create_app()"
+# Change to the src directory where the code actually lives
+cd /opt/render/project/src
 
-# Tell Gunicorn to do the same, but also explicitly add the current
-# directory to the Python path to resolve any ambiguity.
-# The format is wsgi:app (filename:variable_name). We will use run.py
-# as the entry point for gunicorn.
+# Now that we are in the right place, the commands will work
+export FLASK_APP=run.py
 
-# Run Flask database migrations first.
 echo "==> Running database migrations..."
 flask db upgrade
 
-# Now, start the Gunicorn server.
 echo "==> Starting Gunicorn server..."
-gunicorn --pythonpath . -w 4 run:app
+gunicorn run:app
